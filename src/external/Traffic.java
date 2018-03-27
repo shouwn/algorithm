f4462f8f4c299a248129d710286c4ae4b9b61f32
shouwn <31586979+shouwn@users.noreply.github.com>
2018-03-27 11:57:40 +0900

    머지 테스트
    머지 테스트

--- src/external/Traffic.java
+++ src/external/Traffic.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Traffic {
+	
+	static final int OVER_TIME = 0;
+	static final int PROCESS_TIME = 1;
 
 	public static void main(String[] args) throws IOException {
 
@@ -12,6 +15,8 @@ public class Traffic {
 		String input = reader.readLine();
 		reader.close();
 		String[] logs = input.replaceAll("[“”]", "").replaceAll("2016-09-15 ", "").split(", ");
+		input = null;
+		int[][] time = new int[logs.length][2];
 		
 		for(int i = 0; i < logs.length; i++) {
 			System.out.println(logs[i]);
@@ -19,23 +24,25 @@ public class Traffic {
 
 	}
 	
-	static class Time{
-		private int overTime; // 응답완료시간(ms)
-		private int processTime; // 처리시간(ms)
+}
+
+class Time{
+	private int overTime; // 응답완료시간(ms)
+	private int processTime; // 처리시간(ms)
+	
+	public Time(String[] arr) {
+		int i = 3600;
 		
-		public Time(String[] arr) {
-			int i = 3600;
-			
-			for(String s : arr[0].split(":")) {
-				overTime += Integer.valueOf(s) * i;
-				i /= 60;
 			}
-			
-			overTime *= 1000;
-			processTime = Integer.valueOf(arr[1].replaceAll("[.s]", ""));
-			
+		for(String s : arr[0].split(":")) {
+			overTime += Integer.valueOf(s) * i;
+			i /= 60;
 		}
 		
+		overTime *= 1000;
+		processTime = Integer.valueOf(arr[1].replaceAll("[.s]", ""));
+		
 	}
-
+	
+	
 }
