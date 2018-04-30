33fb3d0ae118aef0e74a9f8f865e0aa3f3791554
shouwn <31586979+shouwn@users.noreply.github.com>
2018-04-30 14:15:19 +0900

    binaryGap 구현

---
+++ src/com/codility/BinaryGap.java
@@ -0,0 +1,41 @@
+package com.codility;
+
+import java.util.Scanner;
+
+public class BinaryGap {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		int n = input;
+		scan.close();
+		
+		boolean start = false;
+		
+		int max = 0;
+		int count = 0;
+		
+		while(!start && input != 0) {
+			int bit = input & 1;
+			input >>>= 1;
+			if(bit == 1) {
+				start = true;
+			}
+		}
+		
+		while(input != 0) {
+			int bit = input & 1;
+			input >>= 1;
+			if(bit == 1) {
+				max = Math.max(count, max);
+				count = 0;
+			}else {
+				count++;
+			}
+		}
+		
+		System.out.println(Integer.toBinaryString(n));
+		System.out.println("count: " + max);
+	}
+
+}
