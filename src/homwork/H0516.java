be9f800c87c53118c7b6f67c64f4c0b4733e981f
shouwn <sjonghyon@gmail.com>
2018-05-25 12:42:44 +0900

    MyScanner 구현

--- src/homwork/H0516.java
+++ src/homwork/H0516.java
@@ -9,20 +9,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class WordInfo {
 	String word;
 	int count;
-	
+
 	public WordInfo(String word) {
 		this.word = word;
 		this.count = 1;
 	}
-	
+
 	public void countUp() {
 		count++;
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
@@ -54,39 +56,108 @@ class WordInfo {
 			return false;
 		return true;
 	}
-	
+
 }
 
+// 정규식을 세팅하면 그 정규식에 맞는 다음 문자열을 찾는 클래스
+class MyScanner implements AutoCloseable{
+
+	private BufferedReader reader;
+	private String regex;
+	private Pattern pattern;
+	private Matcher matcher;
+
+	public void setReader(BufferedReader reader) {
+		this.reader = reader;
+	}
+	public void setRegex(String regex) throws IOException {
+		this.regex = regex;
+		pattern = Pattern.compile(this.regex);
+		matcher = pattern.matcher(reader.readLine());
+	}
+
+	// 정규식에 해당하는 문자열이 있는지 확인하는 메소드
+	public boolean find() throws IOException {
+		if(matcher.find()) {
+			return true;
+		}
+		else {
+			String line;
+			while(!matcher.find()) {
+				line = reader.readLine();
+				if(line == null)
+					return false;
+				matcher = pattern.matcher(line);
+			}
+
+			return true;
+		}
+	}
+
+	// 찾은 문자열을 return 해주는 메소드
+	public String read() throws IOException{
+
+		return matcher.group(0);
+	}
+
+	@Override
+	public void close() throws IOException {
+		reader.close();
+	}
+}
+
+class MyScannerFactory{
+	public static MyScanner makeMyScanner(String filePath, String regex) 
+			throws FileNotFoundException, IOException{
+
+		BufferedReader reader = 
+				new BufferedReader(new FileReader(new File(filePath)));
+		MyScanner scan = new MyScanner();
+		scan.setReader(reader);
+		scan.setRegex(regex);
+		return scan;
+	}
+}
 
 public class H0516 {
-	
+
 	List<WordInfo> letterList = new ArrayList<>();
 	Map<WordInfo, Integer> letterMap = new HashMap<>();
-	
+
 	public static void main(String[] args) throws IOException {
+		scanTxt();
+	}
+
+	public static void solution1() throws FileNotFoundException, IOException {
 
 	}
-	
-	public static void solution1() throws FileNotFoundException, IOException {
-	
-	}
-	
+
 	public static void solution2() throws FileNotFoundException, IOException {
 
 	}
-	
+
 	public static void solution3() throws FileNotFoundException, IOException {
 
 	}
-	
-	public static void scanTxt() throws FileNotFoundException, IOException {
-		try(BufferedReader reader = 
-				new BufferedReader(new FileReader(new File("")))) {
-			
-			String regex = "[^A-Za-z]";
-			
-			System.out.println(reader.readLine().replaceAll(regex, ""));
+
+	public static void scanTxt() throws FileNotFoundException, IOException{
+		String regex = "[A-Za-z]+";
+		try(MyScanner scan = MyScannerFactory.makeMyScanner("shakespeare.txt", regex)){
+
+			while(scan.find()) {
+				System.out.println(scan.read());
+			}
 		}
 	}
-	
+
+	public static int getMin(int[] arr) {
+		int min = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(min > arr[i])
+				min = arr[i];
+		}
+
+		return min;
+	}
+
 }
