c33c4f34e384c16870a24ca4a99575dea618aa23
shouwn <31586979+shouwn@users.noreply.github.com>
2018-05-28 16:23:54 +0900

    1번 품

--- src/external/Q2.java
+++ src/external/Q2.java
@@ -215,63 +215,64 @@ public class Q2 {
 		}
 		return lines;
 	}
-}
+	
+	static class MyScanner implements AutoCloseable{
 
-class MyScanner implements AutoCloseable{
+		private BufferedReader reader;
+		private String regex;
+		private Pattern pattern;
+		private Matcher matcher;
 
-	private BufferedReader reader;
-	private String regex;
-	private Pattern pattern;
-	private Matcher matcher;
-
-	public void setReader(BufferedReader reader) {
-		this.reader = reader;
-	}
-	public void setRegex(String regex) throws IOException {
-		this.regex = regex;
-		pattern = Pattern.compile(this.regex);
-		matcher = pattern.matcher(reader.readLine());
-	}
-
-	// 정규식에 해당하는 문자열이 있는지 확인하는 메소드
-	public boolean find() throws IOException {
-		if(matcher.find()) {
-			return true;
+		public void setReader(BufferedReader reader) {
+			this.reader = reader;
 		}
-		else {
-			String line;
-			while(!matcher.find()) {
-				line = reader.readLine();
-				if(line == null)
-					return false;
-				matcher = pattern.matcher(line);
+		public void setRegex(String regex) throws IOException {
+			this.regex = regex;
+			pattern = Pattern.compile(this.regex);
+			matcher = pattern.matcher(reader.readLine());
+		}
+
+		// 정규식에 해당하는 문자열이 있는지 확인하는 메소드
+		public boolean find() throws IOException {
+			if(matcher.find()) {
+				return true;
 			}
+			else {
+				String line;
+				while(!matcher.find()) {
+					line = reader.readLine();
+					if(line == null)
+						return false;
+					matcher = pattern.matcher(line);
+				}
 
-			return true;
+				return true;
+			}
+		}
+
+		// 찾은 문자열을 return 해주는 메소드
+		public int read() throws IOException{
+
+			return Integer.valueOf(matcher.group(0));
+		}
+
+		@Override
+		public void close() throws IOException {
+			reader.close();
 		}
 	}
 
-	// 찾은 문자열을 return 해주는 메소드
-	public int read() throws IOException{
+	static class MyScannerFactory{
+		public static MyScanner makeMyScanner(String regex) 
+				throws FileNotFoundException, IOException{
 
-		return Integer.valueOf(matcher.group(0));
-	}
-
-	@Override
-	public void close() throws IOException {
-		reader.close();
-	}
-}
-
-class MyScannerFactory{
-	public static MyScanner makeMyScanner(String regex) 
-			throws FileNotFoundException, IOException{
-
-		BufferedReader reader = 
-				new BufferedReader(new InputStreamReader(System.in));
-		MyScanner scan = new MyScanner();
-		scan.setReader(reader);
-		scan.setRegex(regex);
-		return scan;
+			BufferedReader reader = 
+					new BufferedReader(new InputStreamReader(System.in));
+			MyScanner scan = new MyScanner();
+			scan.setReader(reader);
+			scan.setRegex(regex);
+			return scan;
+		}
 	}
 }
+
